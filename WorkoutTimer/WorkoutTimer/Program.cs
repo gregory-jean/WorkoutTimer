6f5ab5594c5cdba5e5171819dc55a97baaccfbd1
gregory-jean <30454441+gregory-jean@users.noreply.github.com>
2019-01-23 23:06:56 -0800

    Initial commit.
    First commit, includes input of length of timer and number of times it repeats. Also beeps after completing the timer each time.

---
+++ WorkoutTimer/WorkoutTimer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Timers;
+using System.Threading;
+
+namespace WorkoutTimer
+{
+    public class Timer
+    {
+        public static void Start(int length, int repeatTimes )
+        {
+            for (var i = 0; i < repeatTimes; i++)
+            {
+                System.Threading.Thread.Sleep(length * 1000);
+
+                Console.Beep();
+                Console.WriteLine((i+1) + " of " + repeatTimes + " sets completed.");
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Take in length of set, reset 
+            Console.WriteLine("How long would you like the timer to be?");
+            int setLength = int.Parse(Console.ReadLine());
+            // Take in number of sets
+            Console.WriteLine("How many sets would you like?");
+            int setNumber = int.Parse(Console.ReadLine());
+            // Apply inputs into timer method
+
+            Console.WriteLine("Press Enter to start.");
+            Console.ReadLine();
+
+            Timer.Start(setLength, setNumber);
+
+
+
+
+
+        }
+    }
+}
