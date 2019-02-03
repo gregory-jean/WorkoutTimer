7b1b24ff902d0a8323ae58abcc51361318ce1e2d
Gregory Jean <gregory.g.jean@gmail.com>
2019-02-02 18:10:55 -0800

    Added feature allowing input from the command line.

--- WorkoutTimer/WorkoutTimer/Program.cs
+++ WorkoutTimer/WorkoutTimer/Program.cs
@@ -9,45 +9,63 @@
             int setLength = 0;
             int setNumber = 0;
 
-            Console.WriteLine();
-            Console.WriteLine("-----------------------------------------------------");
-            Console.WriteLine("This is an interval timer");
-            Console.WriteLine("You will enter the length of the sets in seconds, and the number of sets desired.");
-            Console.WriteLine("Use the numeric keys to enter your answers.");
-            Console.WriteLine("-----------------------------------------------------");
-            Console.WriteLine();
+            string setLengthString = string.Empty;
+            string setNumberString = string.Empty;
+
+            bool setLengthGiven = false;
+            bool setNumberGiven = false;
+
+            if (args.Length == 2)
+            {
+                setLengthString = args[0];
+                setNumberString = args[1];
+                setLengthGiven = true;
+                setNumberGiven = true;
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("-----------------------------------------------------");
+                Console.WriteLine("This is an interval timer");
+                Console.WriteLine("You will enter the length of the sets in seconds, and the number of sets desired.");
+                Console.WriteLine("Use the numeric keys to enter your answers.");
+                Console.WriteLine("-----------------------------------------------------");
+                Console.WriteLine();
+            }
 
             // Take in length of sets, verify entry.
             do
             {
-                Console.WriteLine("How long would you like the sets to be? (in seconds)");
-                string setLengthString = Console.ReadLine();
-
                 if (!int.TryParse(setLengthString, out setLength))
                 {
-                    Console.WriteLine();
-                    Console.WriteLine("Invalid entry, type in the length of sets desired using the numberic key. (in seconds)");
+                    if (setLengthGiven)
+                    {
+                        Console.WriteLine("Invalid entry, type in the length of sets desired using the numberic key. (in seconds)");
+                        Console.WriteLine();
+                    }
+
+                    Console.WriteLine("How long would you like the sets to be? (in seconds)");
+                    setLengthString = Console.ReadLine();
+                    setLengthGiven = true;
                     Console.WriteLine();
                 }
             }
             while (setLength == 0);
 
-            Console.WriteLine();
-            Console.WriteLine("Set length set to " + setLength + " seconds.");
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-
             // Take in number of sets, verify entry.
             do
             {
-                Console.WriteLine("How many sets would you like?");
-                string setNumberString = Console.ReadLine();
-
                 if (!int.TryParse(setNumberString, out setNumber))
                 {
-                    Console.WriteLine();
-                    Console.WriteLine("Invalid entry, input number of sets desired using the numeric keys");
+                    if (setNumberGiven)
+                    {
+                        Console.WriteLine("Invalid entry, input number of sets desired using the numeric keys");
+                        Console.WriteLine();
+                    }
+
+                    Console.WriteLine("How many sets would you like?");
+                    setNumberString = Console.ReadLine();
+                    setNumberGiven = true;
                     Console.WriteLine();
                 }
             }
