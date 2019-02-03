b926692ef048ef237dff9ed38aad7e465e92e2b3
Gregory Jean <gregory.g.jean@gmail.com>
2019-02-02 18:47:47 -0800

    Display progress as timer runs.

--- WorkoutTimer/WorkoutTimer/SetTimer.cs
+++ WorkoutTimer/WorkoutTimer/SetTimer.cs
@@ -1,21 +1,34 @@
 ﻿namespace WorkoutTimer
 {
     using System;
+    using System.Diagnostics;
     using System.Threading;
 
     public class SetTimer
     {
         public static void Start(int length, int repeatTimes)
         {
+            int count;
             for (var i = 0; i < repeatTimes; i++)
             {
-                Thread.Sleep(TimeSpan.FromSeconds(length));
+                Console.WriteLine($"Starting set {i + 1}");
+                count = 0;
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                while(stopwatch.Elapsed.TotalSeconds < length)
+                {
+                    // Print a "." to the screen every second
+                    if(stopwatch.Elapsed.TotalMilliseconds / 1000 > count)
+                    {
+                        Console.Write(".");
+                        count++;
+                    }
+                }
+                Console.WriteLine();
 
                 Console.Beep();
                 Console.WriteLine((i + 1) + " of " + repeatTimes + " sets completed.");
             }
         }
-
     }
 }
 
